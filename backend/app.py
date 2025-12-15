a6438e817ec7e95ba36962193ffdac60e0b54021
Parnika110805 <parnikanagraj11@gmail.com>
2025-12-15 09:57:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 from PIL import Image
 
 SUPABASE_URL = "https://cxkojfhujepehbdhxiqq.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4a29qZmh1amVwZWhiZGh4aXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MDc5NjgsImV4cCI6MjA4MTI4Mzk2OH0.IiyiIsRXUoX2W5s7oB-p4mHR3FSoKU6GadRkgtt8cGk"
+SUPABASE_KEY = ""
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
