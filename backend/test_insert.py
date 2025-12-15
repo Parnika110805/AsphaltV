072630a74c5e64ec7f7688fe58e6a03ff3684c5f
Parnika110805 <parnikanagraj11@gmail.com>
2025-12-15 09:57:52 +0530

    Update test_insert.py

--- backend/test_insert.py
+++ backend/test_insert.py
@@ -1,8 +1,7 @@
 from supabase import create_client
 
 url = "https://cxkojfhujepehbdhxiqq.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4a29qZmh1amVwZWhiZGh4aXFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MDc5NjgsImV4cCI6MjA4MTI4Mzk2OH0.IiyiIsRXUoX2W5s7oB-p4mHR3FSoKU6GadRkgtt8cGk"
-
+key = ""
 supabase = create_client(url, key)
 
 data = {
